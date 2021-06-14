e3eda2c58e23477e2cdad16920945056b07f906f
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2021-06-14 15:05:20 +0300

    second try

--- src/main/java/servlets/HelloWorldServlet.java
+++ src/main/java/servlets/HelloWorldServlet.java
@@ -1,23 +1,44 @@
 package servlets;
 
-import javax.servlet.GenericServlet;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class HelloWorldServlet extends GenericServlet {
+@WebServlet("/")
+public class HelloWorldServlet extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
 
     @Override
-    public void service(ServletRequest request, ServletResponse response) throws IOException {
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.service(req, resp);
+    }
 
-        response.setContentType("text/html");
-        PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<body>");
-        out.println("<h1>Hello Servlets World <h1>");
-        out.println("</body>");
-        out.println("</html>");
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.setContentType("text/html");
+        resp.setCharacterEncoding("UTF-8");
 
+        PrintWriter writer = resp.getWriter();
+
+        writer.println("<html>");
+        writer.println(
+                "<h1 align=\"center\">" +
+                        "HelloWorldServlet" +
+                        "</h1>"
+        );
+        writer.println("</html>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
     }
 }
