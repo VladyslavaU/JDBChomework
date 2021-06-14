012e6206f049bf261861e5f7078dd53c5bb912dc
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2021-06-14 16:40:06 +0300

    form

---
+++ src/main/java/servlets/FormServlet.java
@@ -0,0 +1,28 @@
+package servlets;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/enterInfo")
+public class FormServlet extends GenericServlet {
+
+    public void service(ServletRequest request, ServletResponse response) throws IOException {
+
+        if (request.getParameter("firstName") != null && request.getParameter("lastName") != null &&
+                request.getParameter("age") != null) {
+            String firstName = request.getParameter("firstName");
+            String lastName = request.getParameter("lastName");
+            String age = request.getParameter("age");
+
+            PrintWriter out = response.getWriter();
+            out.println("The first name is: " + firstName);
+            out.println("The last name is: " + lastName);
+            out.println("The age is: " + age);
+        }
+
+    }
+}
