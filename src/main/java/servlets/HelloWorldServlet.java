d2ff872cfda650cc53f0c36f6339f6bab03e0124
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2021-06-14 14:54:28 +0300

    helloServlets

---
+++ src/main/java/servlets/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package servlets;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloWorldServlet extends GenericServlet {
+
+    @Override
+    public void service(ServletRequest request, ServletResponse response) throws IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<body>");
+        out.println("<h1>Hello Servlets World <h1>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
+}
