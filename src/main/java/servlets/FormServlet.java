8a27fb241765dfd438844e590f20702dede03f26
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2021-06-15 13:04:21 +0300

    once more

--- src/main/java/servlets/FormServlet.java
+++ src/main/java/servlets/FormServlet.java
@@ -4,10 +4,13 @@ import javax.servlet.GenericServlet;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-//@WebServlet("/enterInfo")
+//@WebServlet(name = "form", urlPatterns = ("/form"))
 public class FormServlet extends GenericServlet {
 
     public void service(ServletRequest request, ServletResponse response) throws IOException {
@@ -24,4 +27,5 @@ public class FormServlet extends GenericServlet {
         }
 
     }
+
 }
