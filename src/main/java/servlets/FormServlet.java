e1e72f02ab99f7f6849583ed4040078c2602d3c3
VLADYSLAVA UVAROVA <vuvarova@outlook.com>
2021-06-15 13:37:08 +0300

    once more

--- src/main/java/servlets/FormServlet.java
+++ src/main/java/servlets/FormServlet.java
@@ -10,8 +10,9 @@ public class FormServlet extends GenericServlet {
 
     @Override
     public void service(ServletRequest request, ServletResponse response) throws IOException {
-        if (request.getParameter("firstName") != null && request.getParameter("lastName") != null &&
-                request.getParameter("age") != null) {
+
+    //    if (request.getParameter("firstName") != null && request.getParameter("lastName") != null &&
+       //         request.getParameter("age") != null) {
             String firstName = request.getParameter("firstName");
             String lastName = request.getParameter("lastName");
             String age = request.getParameter("age");
@@ -20,7 +21,7 @@ public class FormServlet extends GenericServlet {
             out.println("The first name is: " + firstName);
             out.println("The last name is: " + lastName);
             out.println("The age is: " + age);
-        }
+   //     }
 
     }
 
